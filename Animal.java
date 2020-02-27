9b2ac2970f3f7dc46477e8ab8bd63d06f3e1b01d
Amy Feldman <amyfeldman99@gmail.com>
2020-02-27 16:19:20 -0500

    moving java files

---
+++ Animal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Animal{
+	String name;
+        int legs;
+
+        Animal(String name, int legs){
+                this.name = name;
+                this.legs = legs;
+
+	}
+
+	int getLegs(){
+		return this.legs;
+	}
+}
+
---
+++ Dog.java
@@ -0,0 +1,18 @@
+//cisc275-spring2020-first-git
+import java.util.*;
+
+public class Dog extends Animal{
+
+	
+
+	Dog(String name, int legs){
+		super(name, legs);
+	}
+
+	public String toString(){
+		return (name + " has " + legs + " legs");
+	}
+
+
+
+}
